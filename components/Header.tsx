ae48cee692d967c28f4dfdb7f52a055cc2b1c6fb
akashdesaimasai <akash.desai3105@gail.com>
2023-11-29 01:10:28 +0530

    signin feature from main page added

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,14 +1,22 @@
-import { UserButton } from "@clerk/nextjs";
+import { SignInButton, SignedOut, UserButton, auth } from "@clerk/nextjs";
 import { ModeToggle } from "./Toggle";
 
 const Header = () => {
+  const user = auth();
+  console.log(user.userId);
   return (
     <header className="bg-gray-100 dark:bg-slate-900 px-3">
       <div className="flex justify-between p-3 items-center">
         <div className="font-bold">Upload Gallery</div>
         <div className="flex space-x-5 items-center">
           <ModeToggle />
-          <UserButton />
+          {!user.userId ? (
+            <SignedOut>
+              <SignInButton afterSignInUrl="/dashboard"></SignInButton>
+            </SignedOut>
+          ) : (
+            <UserButton />
+          )}
         </div>
       </div>
     </header>
