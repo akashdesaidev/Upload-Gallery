7ce0a56ba37ba60463aafb4ca2af275ca86ad561
akashdesaimasai <akash.desai3105@gail.com>
2023-11-29 16:14:57 +0530

    HomePage is ready

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,45 @@
+import { Button } from "@/components/ui/button";
+import { MoveRight } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="">
-         
-       </main>
-  )
+      <div className=" flex flex-col md:flex-row">
+        <div className=" p-6 flex-1 items-center">
+          <h1 className="flex items-center text-5xl ">
+            Welcome to Upload Gallery.
+          </h1>
+          <h2 className="text-2xl mt-10">
+            Streamline your life and business with an all-in-one solution. From
+            images to documents, we've got you covered in one place.
+          </h2>
+          <p className="mt-4 text-sm">
+            Discover the convenience of having all your personal and business
+            essentials in one centralized hub. From managing your tasks,
+            projects, and appointments to storing important documents and
+            contacts, our all-in-one platform provides a seamless solution. Stay
+            organized, boost productivity, and simplify your life with our
+            comprehensive suite of tools. Explore a smarter way to handle your
+            day-to-day activities – it's all here, in one place, just for you.
+          </p>
+          <Link href="/dashboard">
+            <Button className="mt-16 ml-16 w-56 gap-2">
+             <span>Try it for free</span> 
+              {<MoveRight />}
+            </Button>
+          </Link>
+        </div>
+        <div className="flex1 m-8">
+          <Image
+            src="/Demo_Img.png"
+            width={700}
+            height={500}
+            alt=""
+          />
+        </div>
+      </div>
+    </main>
+  );
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -3,6 +3,7 @@ import { ModeToggle } from "./Toggle";
 import { dark } from "@clerk/themes";
 import Image from "next/image";
 import { Box, FileIcon } from "lucide-react";
+import Link from "next/link";
 
 const Header = () => {
   const user = auth();
@@ -10,10 +11,12 @@ const Header = () => {
   return (
     <header className="bg-gray-100 dark:bg-slate-900 px-3">
       <div className="flex justify-between p-3 items-center">
+        <Link href="/">
         <div className="flex font-bold items-center gap-2">
           <Box/>
           <span>Upload Gallery</span>
         </div>
+        </Link>
         <div className="flex space-x-5 items-center">
           <ModeToggle />
           {!user.userId ? (
